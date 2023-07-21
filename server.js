25a13e9ce261da02dd52ba80c108801800b8b4a8
Tonny Oseko <tonnyoseko0@gmail.com>
2023-07-21 15:32:15 +0300

    FIX: remove unused dependency

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 // server.js
 const express = require("express");
 const axios = require("axios");
-
-
-dotenv.config();
+const makeApiRequest = require("./makeApiRequest");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,17 +11,8 @@ app.use(express.json());
 // Proxy route
 app.get("/motivation", async (req, res) => {
   try {
-    const response = await axios.get(`${API_BASE_URL}motivation`, {
-      headers: {
-        "X-RapidAPI-Key": API_KEY,
-        "X-RapidAPI-Host": "olato-quotes.p.rapidapi.com",
-        "Access-Control-Allow-Origin": "*", // Allow requests from any origin (for testing purposes)
-      },
-      params: {
-        quotes: "random quotes",
-      },
-    });
-    res.json(response.data);
+    const response = await makeApiRequest("motivation");
+    res.json(response);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -32,17 +21,8 @@ app.get("/motivation", async (req, res) => {
 
 app.get("/success", async (req, res) => {
   try {
-    const response = await axios.get(`${API_BASE_URL}success`, {
-      headers: {
-        "X-RapidAPI-Key": API_KEY,
-        "X-RapidAPI-Host": "olato-quotes.p.rapidapi.com",
-        "Access-Control-Allow-Origin": "*", // Allow requests from any origin (for testing purposes)
-      },
-      params: {
-        quotes: "random quotes",
-      },
-    });
-    res.json(response.data);
+    const response = await makeApiRequest("success");
+    res.json(response);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
